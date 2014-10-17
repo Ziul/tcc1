bd7b5d68d56ef12ffc7b4c84d908e9b93739d45a
Luiz Oliveira <ziuloliveira@gmail.com>
2014-10-17 13:32:08 -0300

    Included exact match

--- editaveis/prototipos/exact.py
+++ editaveis/prototipos/exact.py
@@ -7,47 +7,29 @@ from math import fabs
 from multiprocessing.pool import ThreadPool as Pool
 _MAX_PEERS = 20
 
-def _cmp(x, y):
-  if x.ratio[0] == y.ratio[0]:
-    if x.ratio[1] == y.ratio[1]:
-      if x.ratio[2] == y.ratio[2]:
-        if x.ratio[3] == y.ratio[3]:
-          return 0
-        elif x.ratio[3] < y.ratio[3]:
-          return -1
-        else:
-          return 1
-      elif x.ratio[2] < y.ratio[2]:
-        return -1
-      else:
-        return 1
-    elif x.ratio[1] < y.ratio[1]:
-      return 1
-    else:
-      return -1
-  elif x.ratio[0] < y.ratio[0]:
-    return -1
-  else:
-    return 1
+
+def _cmp(pack,other):
+        return pack.name < other.name
 
 def ThreadAlign(k):
     pack = _args[0]
-    item = Pack()
-    item.name = k
-    alinhamento = sw.align(pack,k)
-    item.ratio = (alinhamento.mismatches + fabs(len(pack)-len(k)) - alinhamento.matches) + len(pack)
-    return item
+    if pack in k:
+        item = Pack()
+        item.name = k
+        item.ratio = 0
+        return item
+    return None
 
 def Alinhamento(pack):
     cache = Cache()
     if _options.single:
         list_app = []
         for k in cache:
-            item = Pack()
-            item.name = k.name
-            alinhamento = sw.align(pack,k.name)
-            item.ratio = [alinhamento.matches, fabs(len(pack)-len(k.name)),alinhamento.score, alinhamento.mismatches] 
-            list_app.append(item)
+            if pack in k.name:
+                item = Pack()
+                item.name = k.name
+                item.ratio = 0
+                list_app.append(item)
         return list_app
     else:
         _pool = Pool(processes=_MAX_PEERS)
@@ -84,8 +66,10 @@ if __name__ == '__main__':
                 lista.extend(matches)
 
     # ultimo = time.time()
-    lista =list(set(lista))
-    lista = sorted(lista,reverse=True,cmp=_cmp)
+    #lista =list(set(lista))
+    lista = filter(None, lista)
+    print len(lista)
+    lista = sorted(lista,cmp=_cmp)
     ultimo = time.time()
     print "{} segundos".format(ultimo-agora)
     for i in lista[:_options.amount]:
